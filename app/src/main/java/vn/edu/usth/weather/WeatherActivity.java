8b9e3c61041d2354da952d690cfa47f474b718a8
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-12-16 01:29:49 +0700

    Upload practical work 7

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,13 +3,18 @@ package vn.edu.usth.weather;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.nfc.Tag;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.widget.ScrollView;
 import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -26,15 +31,53 @@ public class WeatherActivity extends AppCompatActivity {
 //        if(savedInstanceState == null){
 //        ForecastFragment forecast = new ForecastFragment();
 //        getSupportFragmentManager().beginTransaction().add(R.id.container, forecast).commit();}
+
+//    public void replaceFragment(Fragment finalFragment){
+//        FragmentManager fragmentManager = this.getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.container, finalFragment);
+//        fragmentTransaction.commit();
+//    }
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        addTabs(viewPager);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void addTabs(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ForecastFragment(), "ForeCast");
+        adapter.addFragment(new WeatherFragment(), "Weather");
+        adapter.addFragment(new WeatherAndForecastFragment(), "both");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager){
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        public CharSequence getPageTitle(int position){
+            return fragmentTitleList.get(position);
+        }
     }
 
-
-    public void replaceFragment(Fragment finalFragment){
-        FragmentManager fragmentManager = this.getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.container, finalFragment);
-        fragmentTransaction.commit();
-    }
 
     @Override
     protected  void onStart(){
